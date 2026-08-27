7cbf76bec1ff0ffdb5d4c65032c32cad473e2e24
qwedus <dkrahd0510@gmail.com>
2026-08-27 17:47:46 +0900

    Update ui.vue

--- web/src/views/ui.vue
+++ web/src/views/ui.vue
@@ -1,3 +1,7 @@
+const SHOW_UNITS = false;
+const SHOW_DIGITAL = false;
+const SHOW_ANALOG = false;
+const SHOW_CAN = false;
 <script setup>
 import { ref, reactive, computed } from 'vue';
 import { views, units, defaults } from '@/service/ui';
@@ -354,8 +358,9 @@ function export_cfg() {
                     </ul>
                 </div>
 
-                <div class="card flex flex-col gap-4" v-if="views.analog.display.telemetry || views.can.display.telemetry">
-                    <div class="font-semibold text-xl">Units</div>
+                 <div class="card flex flex-col gap-4">
++                <div class="card flex flex-col gap-4" v-if="SHOW_UNITS">
+                     <div class="font-semibold text-xl">Units</div>
                     <InputGroup class="mt-2">
                         <InputText id="new_name" v-model="new_unit.name" placeholder="Name" />
                         <InputText id="new_unit" v-model="new_unit.unit" placeholder="Unit" />
@@ -367,7 +372,7 @@ function export_cfg() {
                 </div>
 
                 <div class="card flex flex-col gap-4">
-                    <div class="card flex flex-col gap-4" v-if="views.digital.display.telemetry">
+                    <div class="card flex flex-col gap-4" v-if="SHOW_DIGITAL">
                     <div class="font-semibold text-xl mb-2">Digital</div>
                     <DataView :value="Object.entries(views.digital.ch)" layout="grid">
                         <template #grid="{ items }">
@@ -389,7 +394,7 @@ function export_cfg() {
 
             <div class="md:w-1/2">
                 <div class="card flex flex-col gap-4">
-                    <div class="card flex flex-col gap-4" v-if="views.analog.display.telemetry">
+                    <div class="card flex flex-col gap-4" v-if="SHOW_ANALOG">
                     <div class="font-semibold text-xl mb-2">Analog</div>
                     <DataView :value="Object.entries(analogs)" layout="grid">
                         <template #grid="{ items }">
@@ -427,7 +432,7 @@ function export_cfg() {
                 </div>
 
                 <div class="card flex flex-col gap-4">
-                    <div class="card flex flex-col gap-4" v-if="views.can.display.telemetry">
+                     <div class="card flex flex-col gap-4" v-if="SHOW_CAN">
                     <div class="font-semibold text-xl mb-2">CAN</div>
                     <Button label="Add Message Decoder" icon="pi pi-plus" @click="can_add" />
                     <DataView v-if="can_views.length" :value="can_views">
